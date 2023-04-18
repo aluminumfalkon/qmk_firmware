89e51723b7e87bf3404ecab166c5f54c87ee677f
aluminumfalkon <robert@rlyons.dev>
2023-04-17 20:51:36 -0400

    Remove tri-layer setting

--- users/aluminumfalkon/aluminumfalkon.c
+++ users/aluminumfalkon/aluminumfalkon.c
@@ -1,10 +1,11 @@
 #include "aluminumfalkon.h"
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  state = update_tri_layer_state(state, _QWERTY_LOWER, _NAV, _QWERTY_ADJUST);
-
-  return state;
-}
+//TODO: Remove this code
+//layer_state_t layer_state_set_user(layer_state_t state) {
+//  state = update_tri_layer_state(state, _QWERTY_LOWER, _NAV, _QWERTY_ADJUST);
+//
+//  return state;
+//}
 
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
